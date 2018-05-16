2051cf00f6adf2a24c4c6645b4fdf61477bcc05a
Ryan Geary <rtgnj42@gmail.com>
2018-05-16 00:51:14 -0400

    add high-pass float in processor

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -55,6 +55,9 @@ public:
     void getStateInformation (MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
+	//==============================================================================
+	float highPassFilterFrequency;
+
 private:
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ParametriceqAudioProcessor)
